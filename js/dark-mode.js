3e281cbf79d91db9e5cf753ccbce7b8642f4ae3c
Šimon Trousil <simon.trousil@seznam.cz>
2022-12-17 22:35:05 +0000

    debugbuttonfck

--- js/dark-mode.js
+++ js/dark-mode.js
@@ -1,9 +1,18 @@
 function toggleMode() {
     var element = document.body;
-    var navBar = document.getElementById("navBar");
+    const navBar = document.getElementById("navBar");
     //var navElem = document.getElementById("navLink1");
     element.classList.toggle("dark-body");
-    navBar.classList.toggle("dark-navigation");
+    //navBar.classList.toggle("dark-navigation");
     //navElem.classList.toggle("dark-nav-a");
-  
-  }
+}
+
+function div1ParaElems() {
+  const div1 = document.getElementById("navBar");
+  const div1Paras = div1.getElementsByTagName("a");
+  const len = div1Paras.length;
+  alert(`There are ${len} paragraph in #div1`);
+  //for (e in div1Paras){
+    //e.classList.toggle("dark-nav-a");
+  //}
+}
